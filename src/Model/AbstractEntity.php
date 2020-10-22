080138aeb859a8ed16cdafb1d36d43b2cec0ecd9
Scwaall <scwaall@gmail.com>
2020-10-22 22:50:27 +0200

    Add $date_add & $date_upd properties to AbstractEntity.

--- src/Model/AbstractEntity.php
+++ src/Model/AbstractEntity.php
@@ -18,4 +18,10 @@ namespace Scwaall\YziPrestaShopModule\Model;
 abstract class AbstractEntity
 {
     use TraitEntity;
+
+    /** @var string $date_add The date when the entity was created. */
+    public $date_add;
+
+    /** @var string $date_upd The date when the entity was updated. */
+    public $date_upd;
 }
