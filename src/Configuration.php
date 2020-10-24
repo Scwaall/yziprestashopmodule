9ffd0d8d99d7908eb789e093533d5fc052d54313
Scwaall <scwaall@gmail.com>
2020-10-24 02:13:50 +0200

    Add a Configuration class.

---
+++ src/Configuration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * @author Scwaall
+ * @copyright Copyright (c) Scwaall - 2020
+ * @package YziPrestaShopModule
+ * @support scwaall@gmail.com
+ */
+
+namespace Scwaall\YziPrestaShopModule;
+
+/**
+ * Class Configuration
+ *
+ * @package Scwaall\YziPrestaShopModule
+ */
+class Configuration
+{
+
+}
