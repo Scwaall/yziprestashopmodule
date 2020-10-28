3e35dbc54971f1897d3e64ccf926deb8602437f8
Scwaall <scwaall@gmail.com>
2020-10-28 22:05:25 +0100

    Add an abstract Updater class.

---
+++ src/AbstractUpdater.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * @author Scwaall
+ * @copyright Copyright (c) Scwaall - 2020
+ * @package YziPrestaShopModule
+ * @support scwaall@gmail.com
+ */
+
+namespace Scwaall\YziPrestaShopModule;
+
+abstract class AbstractUpdater
+{
+
+}
