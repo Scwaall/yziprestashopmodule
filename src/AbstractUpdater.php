f1f2cd6e572f06840757f9eee701957616094570
Scwaall <scwaall@gmail.com>
2020-10-29 23:03:29 +0100

    Add methods to update a module.

--- src/AbstractUpdater.php
+++ src/AbstractUpdater.php
@@ -10,7 +10,63 @@
 
 namespace Scwaall\YziPrestaShopModule;
 
+/**
+ * Class AbstractUpdater
+ *
+ * @package Scwaall\YziPrestaShopModule
+ */
 abstract class AbstractUpdater
 {
+    /** @var \Module $module The module's instance. */
+    private $module;
 
+    /**
+     * Updates the module.
+     *
+     * @return bool
+     */
+    abstract public function update();
+
+    /**
+     * AbstractUpdater constructor.
+     *
+     * @param \Module $module The module's instance.
+     */
+    public function __construct(\Module $module)
+    {
+        $this->setModule($module);
+    }
+
+    /**
+     * Gets an instance of an Updater object.
+     *
+     * @param \Module $module The module's instance.
+     * @return static
+     */
+    public static function getInstance(\Module $module)
+    {
+        return new static($module);
+    }
+
+    /**
+     * Gets the module's instance.
+     *
+     * @return \Module
+     */
+    public function getModule()
+    {
+        return $this->module;
+    }
+
+    /**
+     * Sets the module's instance.
+     *
+     * @param \Module $module The module's instance.
+     * @return $this
+     */
+    private function setModule(\Module $module)
+    {
+        $this->module = $module;
+        return $this;
+    }
 }
